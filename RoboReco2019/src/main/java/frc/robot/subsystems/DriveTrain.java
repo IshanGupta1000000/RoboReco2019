cb4dc429d92e39ce6fc13b6126fd46b08ebf4906
Ranit Gupta <ranitgupta06@gmail.com>
2019-09-02 18:52:24 -0500

    next update

--- RoboReco2019/src/main/java/frc/robot/subsystems/DriveTrain.java
+++ RoboReco2019/src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -1,34 +1,39 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj.Talon;
+import edu.wpi.first.wpilibj.Victor;
 import edu.wpi.first.wpilibj.command.Subsystem;
 import frc.robot.RobotMap;
 import frc.robot.commands.TankDrive;
+import com.ctre.phoenix.*;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import com.ctre.phoenix.motorcontrol.can.VictorSPX;
 
 public class DriveTrain extends Subsystem {
-    private Talon motorLeft1 = new Talon(RobotMap.motorLeft1ID);
-    private Talon motorLeft2 = new Talon(RobotMap.motorLeft2ID);
-    private Talon motorLeft3 = new Talon(RobotMap.motorLeft3ID);
-    private Talon motorRight1 = new Talon(RobotMap.motorRight1ID);
-    private Talon motorRight2 = new Talon(RobotMap.motorRight2ID);
-    private Talon motorRight3 = new Talon(RobotMap.motorRight3ID);
+    private TalonSRX motorLeft1 = new TalonSRX(RobotMap.motorLeft1ID);
+    private VictorSPX motorLeft2 = new VictorSPX(RobotMap.motorLeft2ID);
+    private VictorSPX motorLeft3 = new VictorSPX(RobotMap.motorLeft3ID);
+    private TalonSRX motorRight1 = new TalonSRX(RobotMap.motorRight1ID);
+    private VictorSPX motorRight2 = new VictorSPX(RobotMap.motorRight2ID);
+    private VictorSPX motorRight3 = new VictorSPX(RobotMap.motorRight3ID);
 
     @Override
     public void initDefaultCommand() {
 
         setDefaultCommand(new TankDrive());
     }
-
+    //talon srx
+    //victor spx
     public void setLeftMotors(double speed) {
         //motorLeft1.setinverted
-        motorLeft1.set(-speed);
-        motorLeft2.set(-speed);
-        motorLeft3.set(-speed);
+        motorLeft1.set(null, -speed);
+        motorLeft2.set(null, -speed);
+        motorLeft3.set(null, -speed);
     }
 
     public void setRightMotors(double speed) {
-        motorRight1.set(speed);
-        motorRight2.set(speed);
-        motorRight3.set(speed);
+        motorRight1.set(null, speed);
+        motorRight2.set(null, speed);
+        motorRight3.set(null, speed);
     }
 }
